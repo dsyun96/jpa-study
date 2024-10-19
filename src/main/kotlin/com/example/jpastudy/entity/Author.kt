e4b50d8f7ab5a773d7b75edfbc036a55cce488a9
dsyun96 <dsyun96@gmail.com>
2024-10-19 23:12:32 +0900

    지침 반영

--- src/main/kotlin/com/example/jpastudy/entity/Author.kt
+++ src/main/kotlin/com/example/jpastudy/entity/Author.kt
@@ -1,12 +1,10 @@
 package com.example.jpastudy.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
+import java.io.Serializable
 
 @Entity
-class Author {
+class Author : Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
@@ -14,4 +12,35 @@ class Author {
     val age: Int = 0
     val genre: String = ""
     val name: String = ""
+
+    @OneToMany(
+        cascade = [CascadeType.ALL],
+        mappedBy = "author",
+        orphanRemoval = true
+    )
+    val books: MutableList<Book> = mutableListOf()
+
+    fun addBook(book: Book) {
+        books.add(book)
+        book.author = this
+    }
+
+    fun removeBook(book: Book) {
+        book.author = null
+        books.remove(book)
+    }
+
+    fun removeBooks() {
+        val iterator = books.iterator()
+
+        while (iterator.hasNext()) {
+            val book = iterator.next()
+            book.author = null
+            iterator.remove()
+        }
+    }
+
+    override fun toString(): String {
+        return "Author(id=$id, age=$age, genre=$genre, name=$name)"
+    }
 }
--- src/main/kotlin/com/example/jpastudy/entity/Book.kt
+++ src/main/kotlin/com/example/jpastudy/entity/Book.kt
@@ -1,17 +1,40 @@
 package com.example.jpastudy.entity
 
 import jakarta.persistence.*
+import java.io.Serializable
 
 @Entity
-class Book {
+class Book : Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0L
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
-    var author: Author = Author()
+    var id: Long = 0L
 
     var isbn: String = ""
     var title: String = ""
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    var author: Author? = Author()
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) {
+            return false
+        }
+        if (this === other) {
+            return true
+        }
+        if (javaClass != other.javaClass) {
+            return false
+        }
+
+        return id == (other as Book).id
+    }
+
+    override fun hashCode(): Int {
+        return 2024
+    }
+
+    override fun toString(): String {
+        return "Book(id=$id, title=$title, isbn=$isbn)"
+    }
 }
