cd74fd84e302bb9a3a83a180422446df38682c56
dsyun96 <dsyun96@gmail.com>
2024-10-20 20:53:08 +0900

    bytecode enhancement를 위한 추가 플러그인 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,14 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.hibernate.orm") version "6.5.2.Final"
 	kotlin("plugin.jpa") version "1.9.25"
+}
+
+hibernate {
+	enhancement {
+
+	}
 }
 
 group = "com.example"
