e864fb1c065c7669b69b9b9b246a6aaec1ee572a
dsyun96 <dsyun96@gmail.com>
2024-10-21 23:29:03 +0900

    jackson, web 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.fasterxml.jackson.core:jackson-annotations")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	runtimeOnly("com.mysql:mysql-connector-j")
