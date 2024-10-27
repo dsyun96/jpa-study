53d4cdcbd3efdeefbafd6655467bf1c425256dc5
dsyun96 <dsyun96@gmail.com>
2024-10-27 15:43:04 +0900

    insertNewBook 테스트 작성

---
+++ src/main/kotlin/com/example/jpastudy/service/BookstoreService.kt
@@ -0,0 +1,26 @@
+package com.example.jpastudy.service
+
+import com.example.jpastudy.entity.Book
+import com.example.jpastudy.repository.AuthorRepository
+import com.example.jpastudy.repository.BookRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class BookstoreService(
+    private val authorRepository : AuthorRepository,
+    private val bookRepository: BookRepository
+) {
+    @Transactional
+    fun insertNewBook() {
+        val author = authorRepository.getReferenceById(4L)
+
+        val book = Book().apply {
+            isbn = "003-JN"
+            title = "History Of Present"
+            this.author = author
+        }
+
+        bookRepository.save(book)
+    }
+}
