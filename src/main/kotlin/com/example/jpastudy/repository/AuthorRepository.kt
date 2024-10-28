4498182ebf57b3f91cb77a88adac7d0f9b5dc47b
dsyun96 <dsyun96@gmail.com>
2024-10-29 00:04:36 +0900

    Sort 미동작 테스트 작성

--- src/main/kotlin/com/example/jpastudy/repository/AuthorRepository.kt
+++ src/main/kotlin/com/example/jpastudy/repository/AuthorRepository.kt
@@ -1,6 +1,7 @@
 package com.example.jpastudy.repository
 
 import com.example.jpastudy.entity.Author
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
@@ -9,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional
 @Repository
 @Transactional(readOnly = true)
 interface AuthorRepository : JpaRepository<Author, Long> {
-    fun fetchAll(): List<Author>
+    fun fetchAll(pageable: Pageable): List<Author>
     fun fetchByNameAndAge(name: String, age: Int): Author
 
     @Query(nativeQuery = true)
--- src/test/kotlin/com/example/jpastudy/Test.kt
+++ src/test/kotlin/com/example/jpastudy/Test.kt
@@ -4,6 +4,9 @@ import com.example.jpastudy.repository.AuthorRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 
 @SpringBootTest
 class Test {
@@ -12,7 +15,12 @@ class Test {
 
     @Test
     fun test() {
-        authorRepository.fetchAll()
         authorRepository.fetchAllNative()
+    }
+
+    @Test
+    fun pageableTest() {
+        val pageable = PageRequest.of(0, 3, Sort.by(Sort.Direction.ASC, "id"))
+        authorRepository.fetchAll(pageable)
     }
 }
